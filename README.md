4054cb05473cc4ac0222ca25933273ec8b797765
Wieger Wesselink <j.w.wesselink@tue.nl>
2021-11-22 11:49:51 +0100

    Add an implementation of conestrip4
    - N.B. The algorithm doesn't finish on a simple example.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CONEstrip
 An implementation of the CONEstrip algorithms using python and z3.
 
-See [The CONEstrip algorithm](https://doi.org/10.1007/978-3-642-33042-1_6) and
-[A propositional CONEstrip algorithm](https://doi.org/10.1007/978-3-319-08852-5_48).
+See [The CONEstrip algorithm](https://doi.org/10.1007/978-3-642-33042-1_6) SMPS 2012, and
+[A propositional CONEstrip algorithm](https://doi.org/10.1007/978-3-319-08852-5_48) IPMU 2014.
--- src/conestrip/cone.py
+++ src/conestrip/cone.py
@@ -2,16 +2,24 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE or http://www.boost.org/LICENSE_1_0.txt)
 
+from typing import Any, Optional, Tuple
 from more_itertools import collapse
+from more_itertools.recipes import flatten
 from z3 import *
 from gambles import *
 from utility import product, sum_rows
 
 
 def conestrip1(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[int], verbose: bool = False) -> bool:
+    """
+    An implementation of formula (1) in 'A Propositional CONEstrip Algorithm', IPMU 2014.
+    """
+
     # check that Omega_Gamma and Omega_Delta are a partition of { 0, ..., |f0|-1 }
     assert(set(Omega_Gamma).isdisjoint(set(Omega_Delta)))
     assert(set(list(range(len(f0)))) == set(Omega_Gamma) | set(Omega_Delta))
+    # check that the gambles in R0 have the correct length
+    assert(all(len(x) == len(f0) for x in flatten(R0)))
 
     # variables
     lambda_ = [Real(f'lambda{k}') for k in range(len(R0))]
@@ -64,9 +72,15 @@ def conestrip1(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[i
 
 
 def conestrip2(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[int], verbose: bool = False) -> bool:
+    """
+    An implementation of formula (2) in 'A Propositional CONEstrip Algorithm', IPMU 2014.
+    """
+
     # check that Omega_Gamma and Omega_Delta are a partition of { 0, ..., |f0|-1 }
     assert(set(Omega_Gamma).isdisjoint(set(Omega_Delta)))
     assert(set(list(range(len(f0)))) == set(Omega_Gamma) | set(Omega_Delta))
+    # check that the gambles in R0 have the correct length
+    assert(all(len(x) == len(f0) for x in flatten(R0)))
 
     # variables
     lambda_ = [Real(f'lambda{k}') for k in range(len(R0))]
@@ -126,9 +140,15 @@ def conestrip2(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[i
 
 
 def conestrip3(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[int], verbose: bool = False) -> bool:
+    """
+    An implementation of formula (3) in 'A Propositional CONEstrip Algorithm', IPMU 2014.
+    """
+
     # check that Omega_Gamma and Omega_Delta are a partition of { 0, ..., |f0|-1 }
     assert(set(Omega_Gamma).isdisjoint(set(Omega_Delta)))
     assert(set(list(range(len(f0)))) == set(Omega_Gamma) | set(Omega_Delta))
+    # check that the gambles in R0 have the correct length
+    assert(all(len(x) == len(f0) for x in flatten(R0)))
 
     # variables
     lambda_ = [Real(f'lambda{k}') for k in range(len(R0))]
@@ -189,6 +209,83 @@ def conestrip3(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[i
         return False
 
 
+def conestrip4(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[int], verbose: bool = False) -> Optional[Tuple[Any, Any, Any]]:
+    """
+    An implementation of formula (4) in 'A Propositional CONEstrip Algorithm', IPMU 2014.
+    """
+
+    # check that Omega_Gamma and Omega_Delta are a partition of { 0, ..., |f0|-1 }
+    assert(set(Omega_Gamma).isdisjoint(set(Omega_Delta)))
+    assert(set(list(range(len(f0)))) == set(Omega_Gamma) | set(Omega_Delta))
+    # check that the gambles in R0 have the correct length
+    assert(all(len(x) == len(f0) for x in flatten(R0)))
+
+    # variables
+    lambda_ = [Real(f'lambda{k}') for k in range(len(R0))]
+    mu = [[Real(f'mu{k}_{i}') for i in range(len(R0[k]))] for k in range(len(R0))]
+    sigma = Real('sigma')
+
+    # constants
+    g = [[[RealVal(R0[k][i][j]) for j in range(len(R0[k][i]))] for i in range(len(R0[k]))] for k in range(len(R0))]
+    f = [RealVal(f0[j]) for j in range(len(f0))]
+
+    # intermediate expressions
+    h = sum_rows(list(product(lambda_[k], sum_rows([product(mu[k][i], g[k][i]) for i in range(len(R0[k]))])) for k in range(len(R0))))
+    goal = simplify(sum(lambda_))
+
+    # 0 <= lambda <= 1
+    lambda_constraints = [0 <= x for x in lambda_] + [x <= 1 for x in lambda_]
+
+    # tau >= 1
+    mu_constraints = [x >= 0 for x in collapse(mu)]
+
+    # sigma >= 1
+    sigma_constraints = [sigma >= 1]
+
+    # main constraints
+    constraints_1 = [goal >= 1]
+    constraints_2 = [h[omega] <= sigma * f[omega] for omega in Omega_Gamma]
+    constraints_3 = [h[omega] >= sigma * f[omega] for omega in Omega_Delta]
+    constraints_4 = list(collapse([[lambda_[k] <= mu[k][i] for i in range(len(R0[k]))] for k in range(len(R0))]))
+
+    if verbose:
+        print('--- variables ---')
+        print(lambda_)
+        print(mu)
+        print(sigma)
+        print('--- constants ---')
+        print('g =', g)
+        print('f =', f)
+        print('h =', h)
+        print('--- constraints ---')
+        print(lambda_constraints)
+        print(mu_constraints)
+        print(sigma_constraints)
+        print(constraints_1)
+        print(constraints_2)
+        print(constraints_3)
+        print(constraints_4)
+
+    optimizer = Optimize()
+    optimizer.add(lambda_constraints + mu_constraints + sigma_constraints + constraints_1 + constraints_2 + constraints_3 + constraints_4)
+    optimizer.maximize(simplify(sum(lambda_)))
+    if optimizer.check() == sat:
+        model = optimizer.model()
+        lambda_solution = [model.evaluate(lambda_[k]) for k in range(len(R0))]
+        mu_solution = [[model.evaluate(mu[k][i]) for i in range(len(R0[k]))] for k in range(len(R0))]
+        sigma_solution = model.evaluate(sigma)
+        if verbose:
+            print('--- solution ---')
+            print('lambda =', lambda_solution)
+            print('mu =', mu_solution)
+            print('sigma =', sigma_solution)
+        print('goal =', model.evaluate(goal))
+        return lambda_solution, mu_solution, sigma_solution
+    else:
+        print("failed to solve")
+        return None
+
+
 if __name__ == "__main__":
     R = parse_cone('''
       4 0 0
@@ -204,7 +301,27 @@ if __name__ == "__main__":
     f = parse_gamble('2 5 8')
     Omega_Gamma = [0, 1]
     Omega_Delta = [2]
+    print('==================')
+    print('=== conestrip1 ===')
+    print('==================')
     result1 = conestrip1(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+
+    print('==================')
+    print('=== conestrip2 ===')
+    print('==================')
     result2 = conestrip2(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+
+    print('==================')
+    print('=== conestrip3 ===')
+    print('==================')
     result3 = conestrip3(R, f, Omega_Gamma, Omega_Delta, verbose=True)
-    print(result1, result2, result3)
+
+    print('results:', result1, result2, result3, '\n')
+
+    print('==================')
+    print('=== conestrip4 ===')
+    print('==================')
+    lambda_solution, mu_solution, sigma_solution = conestrip4(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+    print('lambda =', lambda_solution)
+    print('mu =', mu_solution)
+    print('sigma =', sigma_solution)
