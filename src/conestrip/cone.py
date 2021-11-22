ceaaa4cbf28245317f4324e9cec658ff4160b2f3
Wieger Wesselink <j.w.wesselink@tue.nl>
2021-11-22 14:14:27 +0100

    Add an alternative implementation of conestrip4
    - The function conestrip4a uses a binary search to find the optimal
    solution. This demonstrates that it can take many iterations before the
    optimum value is found (if that happens at all).

--- src/conestrip/cone.py
+++ src/conestrip/cone.py
@@ -286,6 +286,113 @@ def conestrip4(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[i
         return None
 
 
+def conestrip4a(R0: Cone, f0: Gamble, Omega_Gamma: List[int], Omega_Delta: List[int], verbose: bool = False) -> Optional[Tuple[Any, Any, Any]]:
+    """
+    An implementation of formula (4) in 'A Propositional CONEstrip Algorithm', IPMU 2014.
+    Instead of maximize, an optimal value is found using a loop.
+    """
+
+    # check that Omega_Gamma and Omega_Delta are a partition of { 0, ..., |f0|-1 }
+    assert(set(Omega_Gamma).isdisjoint(set(Omega_Delta)))
+    assert(set(list(range(len(f0)))) == set(Omega_Gamma) | set(Omega_Delta))
+    # check that the gambles in R0 have the correct length
+    assert(all(len(x) == len(f0) for x in flatten(R0)))
+
+    # variables
+    lambda_ = [Real(f'lambda{k}') for k in range(len(R0))]
+    mu = [[Real(f'mu{k}_{i}') for i in range(len(R0[k]))] for k in range(len(R0))]
+    sigma = Real('sigma')
+
+    # constants
+    g = [[[RealVal(R0[k][i][j]) for j in range(len(R0[k][i]))] for i in range(len(R0[k]))] for k in range(len(R0))]
+    f = [RealVal(f0[j]) for j in range(len(f0))]
+
+    # intermediate expressions
+    h = sum_rows(list(product(lambda_[k], sum_rows([product(mu[k][i], g[k][i]) for i in range(len(R0[k]))])) for k in range(len(R0))))
+    goal = simplify(sum(lambda_))
+
+    # 0 <= lambda <= 1
+    lambda_constraints = [0 <= x for x in lambda_] + [x <= 1 for x in lambda_]
+
+    # tau >= 1
+    mu_constraints = [x >= 0 for x in collapse(mu)]
+
+    # sigma >= 1
+    sigma_constraints = [sigma >= 1]
+
+    # main constraints
+    constraints_2 = [h[omega] <= sigma * f[omega] for omega in Omega_Gamma]
+    constraints_3 = [h[omega] >= sigma * f[omega] for omega in Omega_Delta]
+    constraints_4 = list(collapse([[lambda_[k] <= mu[k][i] for i in range(len(R0[k]))] for k in range(len(R0))]))
+    constraints = lambda_constraints + mu_constraints + sigma_constraints + constraints_2 + constraints_3 + constraints_4
+
+    if verbose:
+        print('--- variables ---')
+        print(lambda_)
+        print(mu)
+        print(sigma)
+        print('--- constants ---')
+        print('g =', g)
+        print('f =', f)
+        print('h =', h)
+        print('--- constraints ---')
+        print(lambda_constraints)
+        print(mu_constraints)
+        print(sigma_constraints)
+        print(constraints_2)
+        print(constraints_3)
+        print(constraints_4)
+
+    # Solve the problem with additional constraint 'goal >= value')
+    def solve(value: float) -> Optional[Tuple[float, Any, Any, Any]]:
+        constraints_1 = [goal >= value]
+        # print('solve', constraints_1)
+
+        solver = Solver()
+        solver.add(constraints + constraints_1)
+        if solver.check() == sat:
+            model = solver.model()
+            lambda_solution = [model.evaluate(lambda_[k]) for k in range(len(R0))]
+            mu_solution = [[model.evaluate(mu[k][i]) for i in range(len(R0[k]))] for k in range(len(R0))]
+            sigma_solution = model.evaluate(sigma)
+            goal_solution = model.evaluate(goal)
+            # print('goal =', goal_solution)
+            return goal_solution, lambda_solution, mu_solution, sigma_solution
+        else:
+            return None
+
+    def as_float(value: Any) -> float:
+        if isinstance(value, int):
+            return float(value)
+        assert isinstance(value, RatNumRef)
+        return value.numerator_as_long() / value.denominator_as_long()
+
+    min_value = 1.0
+    max_value = float(len(lambda_))  # the maximum possible value of goal
+    print(f'interval: [{min_value, max_value}]')
+    solution = solve(1)
+    if not solution:
+        return None
+    min_value = as_float(solution[0])
+    while min_value < max_value:
+        print(f'interval: [{min_value, max_value}]')
+        value = (min_value + max_value) / 2
+        sol = solve(value)
+        if sol:
+            min_value = as_float(sol[0])
+            solution = sol
+        else:
+            max_value = as_float(sol[0])
+
+    if verbose:
+        print('--- solution ---')
+        print('goal =', min_value)
+        print('lambda =', solution[1])
+        print('mu =', solution[2])
+        print('sigma =', solution[3])
+    return solution[1], solution[2], solution[3],
+
+
 if __name__ == "__main__":
     R = parse_cone('''
       4 0 0
@@ -319,9 +426,9 @@ if __name__ == "__main__":
     print('results:', result1, result2, result3, '\n')
 
     print('==================')
-    print('=== conestrip4 ===')
+    print('=== conestrip4a ===')
     print('==================')
-    lambda_solution, mu_solution, sigma_solution = conestrip4(R, f, Omega_Gamma, Omega_Delta, verbose=True)
-    print('lambda =', lambda_solution)
-    print('mu =', mu_solution)
-    print('sigma =', sigma_solution)
+    lambda_solution, mu_solution, sigma_solution = conestrip4a(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+    # print('lambda =', lambda_solution)
+    # print('mu =', mu_solution)
+    # print('sigma =', sigma_solution)
