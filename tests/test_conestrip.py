8da09969e44702bd2e128cf6c9afb4b5b0223ba7
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-01-21 11:53:08 +0100

    Update test

--- tests/test_conestrip.py
+++ tests/test_conestrip.py
@@ -3,10 +3,23 @@
 # (See accompanying file LICENSE or http://www.boost.org/LICENSE_1_0.txt)
 
 from unittest import TestCase
-
+from conestrip.cone import parse_cone
 
 class Test(TestCase):
     def stub(self):
+        R = parse_cone('''
+          1 0 0
+          0 1 0
+          0 0 1
+          1 0 1
+          0 1 1
+          1 1 0
+        ''')
+
+        for g in R[0]:
+            print(g)
+
+
         self.assertTrue(True)
 
 
