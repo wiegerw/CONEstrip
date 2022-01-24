d93b749b70fc34669da2dd8cc610b58c6b95d2bb
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-01-24 14:51:52 +0100

    Updated example

--- examples/examples.py
+++ examples/examples.py
@@ -3,6 +3,12 @@ import inspect
 
 
 def info(poly):
+    print('- print the inequalities')
+    print([list(x) for x in poly.get_inequalities()])
+
+    print('- print the generators')
+    print([list(x) for x in poly.get_generators()])
+
     print('- for each face, list adjacent faces')
     print([list(x) for x in poly.get_adjacency()])
 
@@ -15,6 +21,8 @@ def info(poly):
     print('- for each vertex, list adjacent faces')
     print([list(x) for x in poly.get_input_incidence()])
 
+    # print('- linsets of the generators')
+    # print(list(poly.get_generators().lin_set))
 
 # Example 1: consider a rectangle with corners (0,0), (3,0), (3,2) and (0,2). It can be defined using the equations:
 #
@@ -127,37 +135,37 @@ def example3():
                      number_type="fraction")
     mat.rep_type = cdd.RepType.INEQUALITY
     # Use INEQUALITY for H - representation and GENERATOR for V-representation.
-    print('--- the input matrix [b -A] ---')
-    print(mat)
+    # print('--- the input matrix [b -A] ---')
+    # print(mat)
 
     poly = cdd.Polyhedron(mat)
-
-    mat.rep_type = cdd.RepType.GENERATOR
-    generators = poly.get_generators()     # the V-representation
-    # For a polyhedron described as P = conv(v_1, …, v_n) + nonneg(r_1, …, r_s), the V-representation matrix is [t V]
-    #where t is the column vector with n ones followed by s zeroes, and V is the stacked matrix of n vertex row vectors
-    # on top of s ray row vectors.
-    print('--- generators (V-representation, contains the vertices) ---')
-    print(generators)
-
-    inequalities = poly.get_inequalities() # the H-representation
-    print('--- inequalities (H-representation, contains the equations) ---')
-    print(inequalities)
-
-    adjacency_list = poly.get_adjacency()
-    print('--- adjacency list ---')
-    print(adjacency_list)
-
-    # H-representation: For each vertex, list adjacent faces. V-representation: For each face, list adjacent vertices.
-    poly.rep_type = cdd.RepType.INEQUALITY
-    incidence = poly.get_incidence()
-    print('--- for each vertex: adjacent faces ---')
-    print(incidence)
-
-    poly.rep_type = cdd.RepType.GENERATOR
-    incidence = poly.get_incidence()
-    print('--- for each face: adjacent vertices ---')
-    print(incidence)
+    #
+    # mat.rep_type = cdd.RepType.GENERATOR
+    # generators = poly.get_generators()     # the V-representation
+    # # For a polyhedron described as P = conv(v_1, …, v_n) + nonneg(r_1, …, r_s), the V-representation matrix is [t V]
+    # #where t is the column vector with n ones followed by s zeroes, and V is the stacked matrix of n vertex row vectors
+    # # on top of s ray row vectors.
+    # print('--- generators (V-representation, contains the vertices) ---')
+    # print(generators)
+    #
+    # inequalities = poly.get_inequalities() # the H-representation
+    # print('--- inequalities (H-representation, contains the equations) ---')
+    # print(inequalities)
+    #
+    # adjacency_list = poly.get_adjacency()
+    # print('--- adjacency list ---')
+    # print(adjacency_list)
+    #
+    # # H-representation: For each vertex, list adjacent faces. V-representation: For each face, list adjacent vertices.
+    # poly.rep_type = cdd.RepType.INEQUALITY
+    # incidence = poly.get_incidence()
+    # print('--- for each vertex: adjacent faces ---')
+    # print(incidence)
+    #
+    # poly.rep_type = cdd.RepType.GENERATOR
+    # incidence = poly.get_incidence()
+    # print('--- for each face: adjacent vertices ---')
+    # print(incidence)
 
     info(poly)
 
