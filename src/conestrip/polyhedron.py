04fea3928089ef32ecb8ad427e8d55cfb182dcce
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-01-28 09:59:09 +0100

    Improved the Polyhedron interface

--- src/conestrip/polyhedron.py
+++ src/conestrip/polyhedron.py
@@ -1,6 +1,29 @@
 import cdd
 
 
+class InEquality(object):
+    def __init__(self, v):
+        self.v = v
+
+    def __str__(self):
+        # prints a * x[i]
+        def term(a, i):
+            if a == 0:
+                return ''
+            if a == 1:
+                return f'x{i}'
+            if a == -1:
+                return f'-x{i}'
+            return f'{a}*x{i}'
+
+        v = self.v
+        n = len(v)
+        b = -v[0]
+        x = filter(None, [term(v[i], i) for i in range(1, n)])
+        lhs = ' + '.join(x)
+        return f'{lhs} = {b}'
+
+
 class Polyhedron(cdd.Polyhedron):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
@@ -17,11 +40,17 @@ class Polyhedron(cdd.Polyhedron):
     def to_V(self):
         self.rep_type = cdd.RepType.GENERATOR
 
-    def vertices(self):
+    def generators(self):
         return [list(x) for x in self.get_generators()]
 
-    def faces(self):
+    def inequalities(self):
         return [list(x) for x in self.get_inequalities()]
+
+    def vertices(self):
+        return [list(x[1:]) for x in self.get_generators()]
+
+    def faces(self):
+        return [InEquality(x) for x in self.get_inequalities() if x]
 
     # for each vertex, list adjacent vertices
     def vertex_vertex_adjacencies(self):
@@ -53,11 +82,17 @@ class Polyhedron(cdd.Polyhedron):
 
     # prints some information about the polyhedron
     def info(self):
-        print('- list the vertices (generators)')
+        print('- list the vertices')
         print(self.vertices())
 
-        print('- list the faces (inequalities)')
-        print(self.faces())
+        print('- list the faces')
+        print(list(map(str, self.faces())))
+
+        print('- list the generators')
+        print(self.generators())
+
+        print('- list the inequalities')
+        print(self.inequalities())
 
         print('- for each face, list adjacent faces')
         print(self.face_face_adjacencies())
