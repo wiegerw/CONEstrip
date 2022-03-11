9f732396deef249d86d1af242974623c9afeba6d
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-03-11 10:18:21 +0100

    Fix examples.py

--- examples/examples.py
+++ examples/examples.py
@@ -5,7 +5,7 @@
 import inspect
 import cdd
 from conestrip.cones import parse_general_cone, parse_gamble, parse_cone_generator
-from conestrip.conestrip import conestrip1, conestrip2, conestrip3, conestrip
+from conestrip.conestrip import conestrip1_solution, conestrip2_solution, conestrip3_solution, conestrip
 from conestrip.polyhedron import Polyhedron
 from conestrip.prevision import calculate_lower_prevision, calculate_lower_prevision_with_slack
 from conestrip.sure_loss import avoids_sure_loss, avoids_sure_loss_with_slack
@@ -104,9 +104,9 @@ def example_conestrip1():
     f = parse_gamble('2 5 8')
     Omega_Gamma = [0, 1, 2]
     Omega_Delta = [0, 1, 2]
-    result1 = conestrip1(R, f, Omega_Gamma, Omega_Delta, verbose=True)
-    result2 = conestrip2(R, f, Omega_Gamma, Omega_Delta, verbose=True)
-    result3 = conestrip3(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+    result1 = conestrip1_solution(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+    result2 = conestrip2_solution(R, f, Omega_Gamma, Omega_Delta, verbose=True)
+    result3 = conestrip3_solution(R, f, Omega_Gamma, Omega_Delta, verbose=True)
 
     print('result1:', result1)
     print('result2:', result2)
